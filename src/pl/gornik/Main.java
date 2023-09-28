a2b36984002dde9d9d4bdca5884acb3d27b85e52
spoconykocur420 <bbartekak777@gmail.com>
2023-09-28 13:46:45 +0200

    dodanie zmiennej]

---
+++ src/pl/gornik/Main.java
@@ -0,0 +1,17 @@
+package pl.gornik;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("bla bla");
+
+        int liczbaa = 15;
+        int liczbab = 21;
+        int sumaliczb = liczbab + liczbaa;
+        System.out.println("Wartość sumy wynosi " + sumaliczb);
+        System.out.println("Wartość sumy wynosi " + sumaliczb);
+
+
+    }
+}
